b68a878904ee0d5099a877beff09a66fdbd3a064
Mustakim <sycospike1@gmail.com>
2023-05-21 16:26:09 +0600

    posted data to server

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,45 +1,87 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const AllToys = () => {
     const toys = useLoaderData()
-    const { name, } = toys;
+    const { name, price,image} = toys;
+    const {user} = useContext(AuthContext);
+
+    const handleAllToys = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const toyName = form.name.value;
+        const sellerName = form.seller?.value ;
+        const category = form.category.value;
+        const price = form.price.value;
+        const quantity = form.quantity.value;
+        const email = user?.email;
+        const myToy = {
+            toyName,
+            email,
+            image,
+            SellerName: sellerName,
+            category: category,
+            price:price,
+            quantity:quantity
+        }
+        console.log(myToy);
+
+        fetch('http://localhost:5000/myToys',{
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(myToy)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                alert('Toy Added Successfully');
+            }
+        })
+
+    }
+
     return (
         <div>
             <h2>all toys are coming : {name}</h2>
             
-                       <form>
-                       <div className="grid grid-cols-2 gap-6">
+                       <form onSubmit={handleAllToys}>
+                       <div className="md:grid md:grid-cols-2 gap-6">
                        <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Toy Name</span>
                                 </label>
-                                <input type="text" placeholder="toy name" className="input input-bordered" />
+                                <input type="text" name="name" defaultValue={name} className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Seller Name</span>
                                 </label>
-                                <input type="text" placeholder="Seller Name" className="input input-bordered" />
+                                <input type="text" name="seller" placeholder="Seller Name" className="input input-bordered" />
                                
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Category</span>
                                 </label>
-                                <input type="text" placeholder="Category" className="input input-bordered" />
+                                <input type="text" name="category" placeholder="Category" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Price</span>
                                 </label>
-                                <input type="text" placeholder="Price" className="input input-bordered" />
+                                <input type="text" name="price" defaultValue={'$'+ price} className="input input-bordered" />
                                
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Quantity</span>
                                 </label>
-                                <input type="text" placeholder="Quantity" className="input input-bordered" />
+                                <input type="text" name="quantity" placeholder="Quantity" className="input input-bordered" />
                                
                             </div>
                        </div>
